838f523f492f7afb4829256cd382daf6f7a562cf
ikay <ikay@gmail.com>
2025-09-05 22:16:46 +0700

    while

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,6 +1,37 @@
 // static NUMBER: i32 = 18;
 
+// use std::thread::sleep;
+// use std::time::Duration;
+
 fn main() {
+
+    
+
+    // let mut i = 0;
+    // let max = 5;
+
+    // while i < max {
+    //     println!("nilai: {i}");
+    //     i += 1;
+
+    //     sleep(Duration::from_secs(1));
+    // }
+
+    // let mut i = 0;
+    // let max = 5;
+
+    // while i < max {
+    //     let mut j = 0;
+    //     let max_inner = i;
+
+    //     while j <= max_inner {
+    //         print!("* ");
+    //         j += 1;
+    //     }
+
+    //     println!();
+    //     i += 1;
+    // }
 
     // let max = 100.0;
     // let string_f = "nilai minimum kelulusan";
